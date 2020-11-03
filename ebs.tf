3b5d813ee44ac22b4c7387eba8ccc919a82465e0
sunishthaa <31092306+sunishthaa@users.noreply.github.com>
2020-11-03 16:25:11 +0530

    Update ebs.tf

--- ebs.tf
+++ ebs.tf
@@ -6,3 +6,11 @@ resource "aws_ebs_volume" "example" {
   availability_zone = "us-east-1a"
   size              = 1
 }
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-0dba2cb6798deb6d8"
+  instance_type = "t2.micro"
+
+    tags = {
+    Name = "test_backend"
+  }
