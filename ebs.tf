def88bf43adb64114ad084ebe9be0df6a8a59806
sunishthaa <31092306+sunishthaa@users.noreply.github.com>
2020-11-09 18:32:33 +0530

    Update ebs.tf

--- ebs.tf
+++ ebs.tf
@@ -19,10 +19,10 @@ resource "aws_ebs_volume" "example" {
   size              = 1
 }
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-0dba2cb6798deb6d8"
   instance_type = "t2.micro"
 
   tags = {
-    Name = data.terraform_remote_state.ec2.outputs.address
+    Name = "secnd"
   }
 }
