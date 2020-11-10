bd3f76791ebbbc0d1c2f94c72b6bf16e3f41f055
sunishthaa <31092306+sunishthaa@users.noreply.github.com>
2020-11-10 09:42:26 +0530

    Update ebs.tf

--- ebs.tf
+++ ebs.tf
@@ -1,23 +1,9 @@
-data "terraform_remote_state" "foo" {
-  backend = "remote"
-
-  config = {
-    organization = "Sunishthaa_org"
-
-    workspaces = {
-      name = "aws-ec2"
-    }
-  }
-}
 
 provider "aws" {
 
     region     = "us-east-1"
 }
-resource "aws_ebs_volume" "example" {
-  availability_zone = "us-east-1a"
-  size              = 1
-}
+
 resource "aws_instance" "web" {
   ami           = "ami-0dba2cb6798deb6d8"
   instance_type = "t2.micro"
